ed269dfce9814b625ee4727ba9117246bd454ff0
Pavel Nep <dazdramagon@gmail.com>
2025-10-21 15:36:04 +0300

    Добавляет карту на главную страницу

---
+++ src/components/map/map.tsx
@@ -0,0 +1,63 @@
+import leaflet from 'leaflet';
+import { useEffect, useRef } from 'react';
+import { useMap } from './useMap.ts';
+import { URL_MARKER_CURRENT, URL_MARKER_DEFAULT, ICON_SIZE, AnchorLocation } from '../../constants.ts';
+import { TypeOffer } from '../../types/offer.ts';
+import 'leaflet/dist/leaflet.css';
+
+type MapProps = {
+  city: TypeOffer;
+  points: TypeOffer[];
+  selectedOfferId: string | null;
+};
+
+const defaultCustomIcon = leaflet.icon({
+  iconUrl: URL_MARKER_DEFAULT,
+  iconSize: [ICON_SIZE, ICON_SIZE],
+  iconAnchor: [AnchorLocation.Horizontal, AnchorLocation.Vertical],
+});
+
+const currentCustomIcon = leaflet.icon({
+  iconUrl: URL_MARKER_CURRENT,
+  iconSize: [ICON_SIZE, ICON_SIZE],
+  iconAnchor: [AnchorLocation.Horizontal, AnchorLocation.Vertical],
+});
+
+
+function Map({ city, points, selectedOfferId }: MapProps): JSX.Element {
+
+  const mapRef = useRef(null);
+  const map = useMap(mapRef, city);
+
+  useEffect(() => {
+    if (map) {
+      const markerLayer = leaflet.layerGroup().addTo(map);
+      points.forEach((point) => {
+        const marker = leaflet.marker({
+          lat: point.location.latitude,
+          lng: point.location.longitude,
+        });
+
+        marker.setIcon(
+          selectedOfferId !== null && point.id === selectedOfferId
+            ? currentCustomIcon
+            : defaultCustomIcon
+        ).addTo(markerLayer);
+      });
+
+      return () => {
+        map.removeLayer(markerLayer);
+      };
+    }
+  }, [map, points, selectedOfferId]);
+
+  return (
+    <section
+      className="cities__map map"
+      style={{ height: '700px' }}
+      ref={ mapRef }
+    />
+  );
+}
+
+export { Map };
--- src/mocks/offers.ts
+++ src/mocks/offers.ts
@@ -8,16 +8,16 @@ export const offers: TypeOffer[] = [
     'price': 182,
     'previewImage': 'img/apartment-01.jpg',
     'city': {
-      'name': 'Brussels',
+      'name': 'Amsterdam',
       'location': {
-        'latitude': 48.85661,
-        'longitude': 2.351499,
+        'latitude': 52.37454,
+        'longitude': 4.897976,
         'zoom': 13
       }
     },
     'location': {
-      'latitude': 48.868610000000004,
-      'longitude': 2.342499,
+      'latitude': 52.3909553943508,
+      'longitude': 4.85309666406198,
       'zoom': 16
     },
     'isFavorite': true,
@@ -31,16 +31,16 @@ export const offers: TypeOffer[] = [
     'price': 405,
     'previewImage': 'img/apartment-02.jpg',
     'city': {
-      'name': 'Paris',
+      'name': 'Amsterdam',
       'location': {
-        'latitude': 48.85661,
-        'longitude': 2.351499,
+        'latitude': 52.37454,
+        'longitude': 4.897976,
         'zoom': 13
       }
     },
     'location': {
-      'latitude': 48.858610000000006,
-      'longitude': 2.330499,
+      'latitude': 52.3609553943508,
+      'longitude': 4.85309666406198,
       'zoom': 16
     },
     'isFavorite': false,
@@ -54,16 +54,16 @@ export const offers: TypeOffer[] = [
     'price': 243,
     'previewImage': 'img/apartment-03.jpg',
     'city': {
-      'name': 'Paris',
+      'name': 'Amsterdam',
       'location': {
-        'latitude': 48.85661,
-        'longitude': 2.351499,
+        'latitude': 52.37454,
+        'longitude': 4.897976,
         'zoom': 13
       }
     },
     'location': {
-      'latitude': 48.834610000000005,
-      'longitude': 2.335499,
+      'latitude': 52.3909553943508,
+      'longitude': 4.929309666406198,
       'zoom': 16
     },
     'isFavorite': false,
@@ -79,14 +79,14 @@ export const offers: TypeOffer[] = [
     'city': {
       'name': 'Amsterdam',
       'location': {
-        'latitude': 48.85661,
-        'longitude': 2.351499,
+        'latitude': 52.37454,
+        'longitude': 4.897976,
         'zoom': 13
       }
     },
     'location': {
-      'latitude': 48.85761,
-      'longitude': 2.358499,
+      'latitude': 52.3809553943508,
+      'longitude': 4.939309666406198,
       'zoom': 16
     },
     'isFavorite': true,
